48ab1e5b4d82efa9b04176e295b6572a260eb4aa
firas <abidliifiras@gmail.com>
2023-01-13 12:23:46 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "dispose d'un écran tactile de 5,5 pouces avec une résolution de 1080 x 1920 pixels. Il est alimenté par un processeur octa-core à 2,0 GHz, accompagné de 4 Go de RAM et de 64 Go de stockage interne (extensible via une carte microSD). Il possède également une caméra arrière de 12 mégapixels avec autofocus et flash LED, ainsi qu'une caméra frontale de 8 mégapixels pour les selfies. Il fonctionne sous le système d'exploitation Android 9.0 et est équipé d'une batterie de 3000 mAh avec la possibilité de la charge rapide. Il dispose également d'un lecteur d'empreintes digitales pour la sécurité et d'un port USB-C pour la synchronisation et la charge. Il est également compatible avec les réseaux 4G LTE pour une connectivité internet haut débit.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
